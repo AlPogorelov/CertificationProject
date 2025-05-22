33110151324097e839fd8f3bb6320e6c47f7900e
AlPogorelov <eczempl@gmail.com>
2025-05-22 15:39:37 +0600

    Созданы модели Сети поставщиков

---
+++ network/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    """Модель Контактов"""
+    email = models.EmailField()
+    country = models.CharField(
+        max_length=100
+    )
+    city = models.CharField(
+        max_length=100
+    )
+    street = models.CharField(
+        max_length=100
+    )
+    house_number = models.CharField(
+        max_length=20
+    )
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+
+class Product:
+    """ Модель продукта """
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Наименование',
+        unique=True
+    )
+    model = models.CharField(
+        max_length=150,
+        verbose_name='Модель',
+        unique=True
+    )
+    release_date = models.DateField(
+        verbose_name='Дата выхода продукта на рынок'
+    )
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Network(models.Model):
+    """Модель сети поставщиков"""
+    FACTORY = 0
+    RETAIL = 1
+    ENTREPRENEUR = 2
+
+    """Уровни поставщиков"""
+    LEVEL_CHOICES = [
+        (FACTORY, 'Завод'),
+        (RETAIL, 'Розничная сеть'),
+        (ENTREPRENEUR, 'Индивидуальный предприниматель'),
+    ]
+
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Наименование',
+        unique=True
+    )
+    level = models.IntegerField(
+        choices=LEVEL_CHOICES,
+        verbose_name='Уровень поставщика'
+    )
+    contact = models.ForeingKey(
+        Contact,
+        verbose_name='Контакты поставщика'
+    )
+    product = models.ManyToManyField(
+        Product,
+        verbose_name='Продукт поставщика'
+    )
+    supplier = models.ForeingKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Поставщик'
+    )
+    debt = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        verbose_name='Задолжность перед поставщиком'
+    )
+    created_at = models.DateField(
+        verbose_name='Дата создания'
+    )
+
+    class Meta:
+        verbose_name = 'Звено сети'
+        verbose_name_plural = 'Звенья сети'
+
+    def __str__(self):
+        return f"{self.get_level_display()}: {self.name}"
+
+    def save(self, *args, **kwargs):
+        """Если поставщика нет значит уровень поставщика FACTORY(Завод)"""
+        if not self.supplier:
+            self.level = self.FACTORY
+        super().save(*args, **kwargs)
